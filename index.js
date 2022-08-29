d766a403884fcc6c692e940cf53f3eed28eeac19
sowmyasree_v <sowmya.s@accubits.com>
2022-08-29 12:46:31 +0530

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 8030;
+const port = process.env.PORT || 6069;
 
 app.get("/", (req, res) => {
   res.send("Hello Pavan sir!");
