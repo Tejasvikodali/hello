c317afb82d00f57454a144c908f61c66f2cc3acf
sowmyasree_v <sowmya.s@accubits.com>
2022-08-29 12:42:30 +0530

    first commit

--- index.js
+++ index.js
@@ -10,4 +10,5 @@ app.listen(port, () => {
   console.log(
     `App listening at mongodb+srv://vidlyuser:vidlypass@cluster0.7ofhy.mongodb.net/?retryWrites=true&w=majority :${port}`
   );
+  console.log("Hello Pavan sir");
 });
